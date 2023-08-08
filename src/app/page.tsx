ef58fad811a3512b1bdacaa1019039d62ac6aaff
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-08 00:29:54 -0700

    Add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from 'next/image';
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-	return <main>sutff</main>;
+  return (
+    <main className="flex flex-col items-center p-10">
+      <section className="w-fit text-center">
+        <h1 className="my-6 text-7xl">Explore the World</h1>
+        <h2 className="text-4xl">
+          Simplicity and ease in planning your adventures.
+        </h2>
+      </section>
+    </main>
+  );
 }

28ece391bea3ea761a0f969bb5afff64ea9a5567
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-07 21:28:43 -0700

    Add prettier tailwind plugin

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex bg-emerald-600 text-gray-200">
+      <h2>Journey</h2>
+      <button>Login</button>
+    </header>
+  );
+}

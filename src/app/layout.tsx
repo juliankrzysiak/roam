5f230bca847d6522b16bc0904b4d79b361c7c3d7
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-07 23:14:39 -0700

    Add footer and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
+import Footer from "@/components/Footer";
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Journey",
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={nunito.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );

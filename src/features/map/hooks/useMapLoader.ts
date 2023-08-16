17f77989fa13b1b8ea8b54c79ddbbc3f225a0bd8
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-15 18:23:44 -0700

    Add map id for travel styling

--- src/features/map/hooks/useMapLoader.ts
+++ src/features/map/hooks/useMapLoader.ts
@@ -16,6 +16,7 @@ export default function useMapLoader() {
       new Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 34.052, lng: -118.243 },
         zoom: 10,
+        mapId: process.env.NEXT_PROCESS_MAP_ID,
       });
     });
   }, []);

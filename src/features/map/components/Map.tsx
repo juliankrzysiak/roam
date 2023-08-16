194d7bd2d64ba5c573e051c6039915cf104e703d
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-15 17:48:25 -0700

    Convert map loader into custom hook

--- src/features/map/components/Map.tsx
+++ src/features/map/components/Map.tsx
@@ -1,25 +1,8 @@
 "use client";
 
-import { Loader } from "@googlemaps/js-api-loader";
-import { useEffect } from "react";
-
-const loader = new Loader({
-  apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
-  version: "weekly",
-});
+import useMapLoader from "../hooks/useMapLoader";
 
 export default function Map() {
-  useEffect(() => {
-    loader.load().then(async () => {
-      const { Map } = (await google.maps.importLibrary(
-        "maps",
-      )) as google.maps.MapsLibrary;
-      new Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      });
-    });
-  });
-
+  useMapLoader();
   return <div className="h-full flex-auto" id="map"></div>;
 }
---
+++ src/features/map/hooks/useMapLoader.ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+  apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+  version: "weekly",
+});
+
+export default function useMapLoader() {
+  useEffect(() => {
+    loader.load().then(async () => {
+      const { Map } = (await google.maps.importLibrary(
+        "maps",
+      )) as google.maps.MapsLibrary;
+
+      new Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: 34.052, lng: -118.243 },
+        zoom: 10,
+      });
+    });
+  }, []);
+}

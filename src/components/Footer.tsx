6adffde72c632a1697e2e3870370f30b4faada95
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-15 17:38:28 -0700

    Change layout styling to fit map

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 flex w-full justify-between bg-emerald-700 p-4 text-lg font-bold text-gray-100">
+    <footer className="flex justify-between bg-emerald-700 p-4 text-lg font-bold text-gray-100">
       <h3>Made by Julian Krzysiak</h3>
       <a href="https://github.com/CastillejaCode/journey">Github</a>
     </footer>

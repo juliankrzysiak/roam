52d36b3507e57a718ec0d937ddc42c19f68d16ea
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-19 23:45:45 -0700

    Add login component

---
+++ src/features/auth/components/Login.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const supabase = createClientComponentClient<any>();
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    router.refresh();
+  };
+
+  return (
+    <section>
+      <input
+        type="email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button onClick={handleSignIn}>Sign In</button>
+    </section>
+  );
+}

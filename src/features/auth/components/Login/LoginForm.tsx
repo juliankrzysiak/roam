e2bfb11bb0d19f84b2f39dced25859451615ec23
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-23 00:13:35 -0700

    Separate component into dialog and login form

---
+++ src/features/auth/components/Login/LoginForm.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Must be a valid email address." }),
+  // Can implement strong password integer using validator.js
+  password: z.string().min(6).max(20),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="your email address" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/features/auth/components/Login/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import LoginForm from "./LoginForm";
+
+export default function Login() {
+  return (
+    <Dialog>
+      <DialogTrigger className="text-2xl font-semibold">Sign In</DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="space-y-8">
+          <DialogTitle className="text-2xl">
+            Log in to your account.
+          </DialogTitle>
+          <LoginForm />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}

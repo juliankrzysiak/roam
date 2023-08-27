579fcb9b926cb9a4494ea01d866eea2142aef394
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-26 23:50:25 -0700

    Add sign up component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import Login from "@/features/auth/components/Login";
+import SignUp from "@/features/auth/components/SignUp";
 
 export default function Header() {
   return (
     <>
       <header className="flex justify-between bg-emerald-600 px-4 py-2 text-gray-50">
         <h2 className="text-4xl font-bold">Journey</h2>
-        <Login />
+        <div className="flex gap-6">
+          <Login />
+          <SignUp />
+        </div>
       </header>
     </>
   );
---
+++ src/features/auth/components/SignUp/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import SignUpForm from "./SignUpForm";
+
+export default function SignUp() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger className="text-2xl font-semibold">Sign Up</DialogTrigger>
+      <DialogContent className="flex flex-col gap-8">
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Create a new account.</DialogTitle>
+        </DialogHeader>
+        <SignUpForm setOpen={setOpen} />
+      </DialogContent>
+    </Dialog>
+  );
+}

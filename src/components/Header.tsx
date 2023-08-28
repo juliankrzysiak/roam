276896d2affd1e442d2cbd0532b727c37e021eb2
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-28 00:57:53 -0700

    Refactor nav for easier future use

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,15 @@
-import Login from "@/features/auth/components/Login";
-import SignUp from "@/features/auth/components/SignUp";
+"use client";
+
 import Link from "next/link";
+import Nav from "./Nav";
 
 export default function Header() {
   return (
-    <>
-      <header className="flex justify-between bg-emerald-600 px-4 py-2 text-gray-50">
-        <Link href="/" className="text-4xl font-bold">
-          Journey
-        </Link>
-        <div className="flex gap-8">
-          <Login />
-          <SignUp />
-        </div>
-      </header>
-    </>
+    <header className="flex justify-between bg-emerald-600 px-4 py-2 text-gray-50">
+      <Link href="/" className="text-4xl font-bold">
+        Journey
+      </Link>
+      <Nav />
+    </header>
   );
 }

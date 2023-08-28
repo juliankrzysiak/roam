cbc17c0e4b3a24361445ec6df68ae6412c52121c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-27 21:53:50 -0700

    Add link to home in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import Login from "@/features/auth/components/Login";
 import SignUp from "@/features/auth/components/SignUp";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <>
       <header className="flex justify-between bg-emerald-600 px-4 py-2 text-gray-50">
-        <h2 className="text-4xl font-bold">Journey</h2>
-        <div className="flex gap-6">
+        <Link href="/" className="text-4xl font-bold">
+          Journey
+        </Link>
+        <div className="flex gap-8">
           <Login />
           <SignUp />
         </div>

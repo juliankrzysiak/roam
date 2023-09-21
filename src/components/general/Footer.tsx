d4a10ff656477822b6fc9f1d0caef33d93dc965c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-20 22:20:08 -0700

    Move footer

---
+++ src/components/general/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between bg-emerald-700 p-4 text-lg font-bold text-gray-100">
+      <h3>Made by Julian Krzysiak</h3>
+      <a href="https://github.com/CastillejaCode/journey">Github</a>
+    </footer>
+  );
+}

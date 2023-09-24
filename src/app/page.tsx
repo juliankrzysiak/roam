08d3848aa203622c3236e8df0c8e9064a364577c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-09-23 23:25:51 -0700

    Add review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
+import Action from "@/features/landing/components/Action";
 import Features from "@/features/landing/components/Features";
 import Hero from "@/features/landing/components/Hero";
+import Reviews from "@/features/landing/components/Reviews";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-8">
+    <main className="flex flex-col items-center gap-20 px-8 lg:px-20 ">
       <Hero />
       <Features />
-      <section className="text-xl">
-        <h1 className="my-6 text-6xl">Testimonials</h1>
-        <p>Planning has never been so fun!</p>
-        <p>I love this service, how is it free!!??</p>
-        <p>Awesome</p>
-        <p>Really good stuff mate, keep it up!</p>
-      </section>
+      <Reviews />
+      <Action />
     </main>
   );
 }

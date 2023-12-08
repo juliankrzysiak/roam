3d0e5c16323bd7f7a075783c70c676ba5de4aada
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-07 16:39:39 -0800

    Click on poi to get name

--- src/features/map/components/Map.tsx
+++ src/features/map/components/Map.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import mapboxgl, { Map } from "mapbox-gl";
+import mapboxgl, { Map as MapType } from "mapbox-gl";
 import { useEffect, useRef, useState } from "react";
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY as string;
 
 export default function Map() {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<Map | null>(null);
+  const map = useRef<MapType | null>(null);
   const [lng, setLng] = useState(-118);
   const [lat, setLat] = useState(34);
   const [zoom, setZoom] = useState(7);
@@ -15,7 +15,7 @@ export default function Map() {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current as HTMLElement,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: "mapbox://styles/mapbox/outdoors-v12",
       center: [lng, lat],
       zoom: zoom,
     });
@@ -25,6 +25,21 @@ export default function Map() {
       setLng(Number(map.current.getCenter().lng.toFixed(4)));
       setLat(Number(map.current.getCenter().lat.toFixed(4)));
       setZoom(Number(map.current.getZoom().toFixed(2)));
+    });
+
+    map.current.on("click", (e) => {
+      const features = map.current?.queryRenderedFeatures(e.point, {
+        layers: ["poi-label"],
+      });
+      if (!features || features.length < 1) return;
+      if (!map.current) return;
+
+      const name = features[0].properties?.name;
+
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`<h1>${name}</h1>`)
+        .addTo(map.current);
     });
   });
 

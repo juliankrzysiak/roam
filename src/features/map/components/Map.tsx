f0840d4f256158aa45cb9bfc49e8de2aaf1f95e3
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-07 18:27:28 -0800

    Implement react library for mapbox

--- src/features/map/components/Map.tsx
+++ src/features/map/components/Map.tsx
@@ -1,47 +1,85 @@
 "use client";
 
-import mapboxgl, { Map as MapType } from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY as string;
+import { useState } from "react";
+import {
+  Map as MapBox,
+  MapLayerMouseEvent,
+  MapboxEvent,
+  Popup,
+} from "react-map-gl";
 
 export default function Map() {
-  const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<MapType | null>(null);
-  const [lng, setLng] = useState(-118);
-  const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(7);
+  // const mapContainer = useRef<HTMLDivElement>(null);
+  // const map = useRef<MapType | null>(null);
+  // const popupRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+  // const [lng, setLng] = useState(-118);
+  // const [lat, setLat] = useState(34);
+  // const [zoom, setZoom] = useState(12);
+  //
+  // useEffect(() => {
+  // if (map.current) return; // initialize map only once
+  // const map = new mapboxgl.Map({
+  // container: mapContainer.current as HTMLElement,
+  // style: "mapbox://styles/mapbox/outdoors-v12",
+  // center: [lng, lat],
+  // zoom: zoom,
+  // });
+  //
+  // map.current.on("move", () => {
+  //   if (!map.current) return;
+  //   setLng(Number(map.current.getCenter().lng.toFixed(4)));
+  //   setLat(Number(map.current.getCenter().lat.toFixed(4)));
+  //   setZoom(Number(map.current.getZoom().toFixed(2)));
+  // });
+  //
+  // map.on("click", (e) => {
+  // const features = map.queryRenderedFeatures(e.point, {
+  // layers: ["poi-label"],
+  // });
+  // if (features.length > 0) {
+  // const feature = features[0];
+  // const popupNode = document.createElement("div");
+  // const popupRoot = createRoot(popupNode);
+  // popupRoot.render(<Popup name={feature.properties} />);
+  //
+  // popupRef.current
+  // .setLngLat(e.lngLat)
+  // .setDOMContent(popupNode)
+  // .addTo(map);
+  // }
+  // });
+  // }, []);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current as HTMLElement,
-      style: "mapbox://styles/mapbox/outdoors-v12",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+  const [showPopup, setShowPopup] = useState(false);
+  const [lngLat, setLngLat] = useState({ lng: -122.4, lat: 37.8 });
 
-    map.current.on("move", () => {
-      if (!map.current) return;
-      setLng(Number(map.current.getCenter().lng.toFixed(4)));
-      setLat(Number(map.current.getCenter().lat.toFixed(4)));
-      setZoom(Number(map.current.getZoom().toFixed(2)));
-    });
+  function handlePopup(e: MapLayerMouseEvent) {
+    setShowPopup(true);
+    const { lng, lat } = e.lngLat;
+    setLngLat({ lng: +lng.toFixed(5), lat: +lat.toFixed(5) });
+  }
 
-    map.current.on("click", (e) => {
-      const features = map.current?.queryRenderedFeatures(e.point, {
-        layers: ["poi-label"],
-      });
-      if (!features || features.length < 1) return;
-      if (!map.current) return;
-
-      const name = features[0].properties?.name;
-
-      new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(`<h1>${name}</h1>`)
-        .addTo(map.current);
-    });
-  });
-
-  return <div className="h-full" ref={mapContainer}></div>;
+  return (
+    <MapBox
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      onClick={handlePopup}
+    >
+      {showPopup && (
+        <Popup
+          longitude={lngLat.lng}
+          latitude={lngLat.lat}
+          closeOnClick={false}
+          onClose={() => setShowPopup(false)}
+        >
+          <h1>sasdasd</h1>
+        </Popup>
+      )}
+    </MapBox>
+  );
 }

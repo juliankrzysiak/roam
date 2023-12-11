ca38c4a90097aaa369b62552eeb7d689be96c65a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-10 22:20:07 -0800

    Add times together correctly

--- src/features/planner/components/index.tsx
+++ src/features/planner/components/index.tsx
@@ -1,6 +1,7 @@
-import { SetStateAction, useState } from "react";
+import { ChangeEvent, SetStateAction, useState } from "react";
 import { PlaceT } from "@/types";
 import Place from "./Place";
+import { add, parse, format, addMinutes } from "date-fns";
 
 interface Props {
   places: PlaceT[];
@@ -8,10 +9,9 @@ interface Props {
 }
 
 export default function Planner({ places, setPlaces }: Props) {
-  const [startTime, setStarttime] = useState("");
-
-  let prev = startTime;
-  let endTime = "0";
+  const [startTime, setStartTime] = useState("08:00");
+  let prev = parse(startTime, "HH:mm", new Date());
+  let endTime;
 
   return (
     <section className="absolute inset-4 left-10 top-1/2 h-5/6 w-4/12  -translate-y-1/2 rounded-xl border-4 border-emerald-600 bg-gray-100 shadow-lg ">
@@ -21,24 +21,24 @@ export default function Planner({ places, setPlaces }: Props) {
         <input
           type="time"
           value={startTime}
-          onChange={(event) => setStarttime(event.target.value)}
+          onChange={(event) => setStartTime(event.target.value)}
         />
       </label>
       {places.map((place, i, arr) => {
         const arrival = prev;
-        const departure = prev + (place.duration ?? 0);
+        const departure = addMinutes(arrival, place.duration);
         prev = departure;
-        if (i === arr.length - 1) endTime = departure;
+        if (i === arr.length - 1) endTime = format(departure, "HH:mm a");
         return (
           <Place
             key={place.id}
             {...place}
-            arrival={arrival}
-            departure={departure}
+            arrival={format(arrival, "HH:mm a")}
+            departure={format(departure, "HH:mm a")}
           />
         );
       })}
-      <span className="flex w-fit flex-col">End Time {endTime}</span>
+      <span className="flex w-fit flex-col">End Time {endTime ?? 0}</span>
     </section>
   );
 }

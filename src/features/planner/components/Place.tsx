d24eb04eee43a7240e4baa9cf96ddcbbbbb91bfd
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-10 20:36:50 -0800

    Add place component

--- src/features/planner/components/Place.tsx
+++ src/features/planner/components/Place.tsx
@@ -1,10 +1,24 @@
-import { PlaceT } from "@/types";
+import { useState } from "react";
+import { add, addMinutes } from "date-fns";
 
-export default function Place({ name, category }: PlaceT) {
+export default function Place({ name, arrival, departure }: any) {
+  const [duration, setDuration] = useState("00:00");
   return (
-    <div key={name}>
+    <div className="my-4 flex ">
+      <div>
+        <div>Arrival {arrival}</div>
+        <label className="flex">
+          Duration
+          <input
+            type="number"
+            value={duration}
+            step={5}
+            onChange={(event) => setDuration(event.target.value)}
+          />
+        </label>
+        <div>Departure {departure}</div>
+      </div>
       <h1>{name}</h1>
-      <h2>{category}</h2>
     </div>
   );
 }

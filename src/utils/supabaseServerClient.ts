77c37dcbc7977db39c029120a8c3616caa70e322
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-18 21:01:41 -0800

    Separate supabase clients

--- src/utils/supabaseServerClient.ts
+++ src/utils/supabaseServerClient.ts
@@ -1,12 +1,5 @@
-import { createBrowserClient, createServerClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-
-export function createSupabaseBrowserClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
-}
 
 export function createSupabaseServerClient() {
   const cookieStore = cookies();

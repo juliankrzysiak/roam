83efdd0733e1fedad9e01d8921323f9134ea395a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-20 00:16:12 -0800

    Add server action to delete place

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -2,11 +2,23 @@
 
 import { revalidateTag, revalidatePath } from "next/cache";
 import { createSupabaseServerClient } from "./supabaseServerClient";
+import { PlaceT } from "@/types";
 
-export async function createPlace(place) {
-  const supabase = createSupabaseServerClient();
+const supabase = createSupabaseServerClient();
+
+export async function createPlace(place: PlaceT) {
   try {
-    const { data, error } = await supabase.from("places").insert(place);
+    const { error } = await supabase.from("places").insert(place);
+    if (error) throw new Error(`Supabase error: ${error.message}`);
+    revalidatePath("/map");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deletePlace(place: PlaceT) {
+  try {
+    const { error } = await supabase.from("places").delete().eq("id", place.id);
     if (error) throw new Error(`Supabase error: ${error.message}`);
     revalidatePath("/map");
   } catch (error) {

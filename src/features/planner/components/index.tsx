fde2ac31e45fb7d4070785c1c7495a2cd68b331b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-20 20:59:28 -0800

    Reimplement planner time adding using database

--- src/features/planner/components/index.tsx
+++ src/features/planner/components/index.tsx
@@ -1,13 +1,9 @@
 import { PlaceT } from "@/types";
 import { add, format, parse } from "date-fns";
-import { SetStateAction, useState } from "react";
 import Place from "./Place";
-import { cookies } from "next/headers";
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { createSupabaseServerClient } from "@/utils/supabaseServerClient";
 
 interface Props {
-  places: any;
+  places: PlaceT[];
 }
 
 // async function getRoute(places: PlaceT[]) {
@@ -23,42 +19,29 @@ interface Props {
 // }
 
 export default async function Planner({ places }: Props) {
-  // const [startTime, setStartTime] = useState("08:00");
-  // let prev = parse(startTime, "HH:mm", new Date());
+  let prev = parse("08:00", "HH:mm", new Date());
   let endTime;
-  console.log(places);
 
   return (
     <section className="absolute inset-4 left-10 top-1/2 h-5/6 w-4/12  -translate-y-1/2 rounded-xl border-4 border-emerald-600 bg-gray-100 shadow-lg ">
       <div className="h-20 border-4 border-b"></div>
       <label className="flex w-fit flex-col">
         Start time
-        {/* <input
-          type="time"
-          value={startTime}
-          onChange={(event) => setStartTime(event.target.value)}
-        /> */}
+        <input type="time" defaultValue={format(prev, "HH:mm")} />
       </label>
-      {/* {places.map((place, i, arr) => {
+      {places.map((place, i, arr) => {
         const arrival = prev;
-        const hours = place.duration.hours;
-        const minutes = place.duration.minutes;
-        const departure = add(arrival, { hours, minutes });
+        const departure = add(arrival, { minutes: place.duration });
         prev = departure;
-        if (i === arr.length - 1) endTime = format(departure, "HH:mm a");
+        if (i === arr.length - 1) endTime = format(departure, "HH:mm");
         return (
           <Place
             key={place.id}
             place={place}
             arrival={arrival}
             departure={departure}
-            places={places}
-            setPlaces={setPlaces}
           />
         );
-      })} */}
-      {places.map((place) => {
-        return <Place key={place.id} place={place} />;
       })}
       <span className="flex w-fit flex-col">End Time {endTime ?? 0}</span>
     </section>
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import { revalidateTag, revalidatePath } from "next/cache";
-import { createSupabaseServerClient } from "./supabaseServerClient";
 import { PlaceT } from "@/types";
+import { revalidatePath } from "next/cache";
+import { createSupabaseServerClient } from "./supabaseServerClient";
 
 const supabase = createSupabaseServerClient();
 
@@ -19,6 +19,23 @@ export async function createPlace(place: PlaceT) {
 export async function deletePlace(place: PlaceT) {
   try {
     const { error } = await supabase.from("places").delete().eq("id", place.id);
+    if (error) throw new Error(`Supabase error: ${error.message}`);
+    revalidatePath("/map");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateDuration(formData: FormData) {
+  const id = formData.get("id");
+  const rawFormData = {
+    duration: formData.get("duration"),
+  };
+  try {
+    const { error } = await supabase
+      .from("places")
+      .update(rawFormData)
+      .eq("id", id);
     if (error) throw new Error(`Supabase error: ${error.message}`);
     revalidatePath("/map");
   } catch (error) {

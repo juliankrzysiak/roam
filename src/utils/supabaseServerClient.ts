4681d163b2a9a13918e19b9a0b67321822ec8880
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-22 23:47:23 -0800

    Remove setting and removing cookies in supabase server client

--- src/utils/supabaseServerClient.ts
+++ src/utils/supabaseServerClient.ts
@@ -12,12 +12,6 @@ export function createSupabaseServerClient(
         get(name) {
           return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
-          cookieStore.set(name, value, options);
-        },
-        remove(name, options) {
-          cookieStore.set(name, "", options);
-        },
       },
     },
   );

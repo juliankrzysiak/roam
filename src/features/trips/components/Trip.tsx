307b42b6cb0c720d49e941ee62ff93bc0727d76b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-23 01:43:42 -0800

    Add server action to delete trip

--- src/features/trips/components/Trip.tsx
+++ src/features/trips/components/Trip.tsx
@@ -1,3 +1,4 @@
+import { deleteTrip } from "@/utils/actions";
 import Link from "next/link";
 
 interface Props {
@@ -5,11 +6,16 @@ interface Props {
   id: number;
 }
 
-export default function Trip({ name, id }: Props) {
+export default async function Trip({ name, id }: Props) {
+  const deleteTripWithId = deleteTrip.bind(null, id);
+
   return (
-    <article>
+    <article className="flex flex-col border ">
       <h1>{name}</h1>
       <Link href={`/map/${id}`}>Check out trip</Link>
+      <form action={deleteTripWithId}>
+        <button>delete trip</button>
+      </form>
     </article>
   );
 }
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -5,60 +5,14 @@ import { revalidatePath } from "next/cache";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
+const cookieStore = cookies();
+const supabase = createClient(cookieStore);
+
+// Create //
+
 export async function createPlace(place: PlaceT) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
   try {
     const { error } = await supabase.from("places").insert(place);
-    if (error) throw new Error(`Supabase error: ${error.message}`);
-    revalidatePath("/map");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function deletePlace(place: PlaceT) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  try {
-    const { error } = await supabase.from("places").delete().eq("id", place.id);
-    if (error) throw new Error(`Supabase error: ${error.message}`);
-    revalidatePath("/map");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function updateDuration(formData: FormData) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const id = formData.get("id");
-  const rawFormData = {
-    duration: formData.get("duration"),
-  };
-  try {
-    const { error } = await supabase
-      .from("places")
-      .update(rawFormData)
-      .eq("id", id);
-    if (error) throw new Error(`Supabase error: ${error.message}`);
-    revalidatePath("/map");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function updateStartTime(formData: FormData) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const rawFormData = {
-    start_time: formData.get("startTime"),
-  };
-  try {
-    const { error } = await supabase
-      .from("days")
-      .update(rawFormData)
-      .eq("id", 3);
     if (error) throw new Error(`Supabase error: ${error.message}`);
     revalidatePath("/map");
   } catch (error) {
@@ -80,4 +34,57 @@ export async function createTrip(formData: FormData) {
   } catch (error) {
     console.log(error);
   }
+}
+
+// Update //
+
+export async function updateDuration(formData: FormData) {
+  const id = formData.get("id");
+  const rawFormData = {
+    duration: formData.get("duration"),
+  };
+  try {
+    const { error } = await supabase
+      .from("places")
+      .update(rawFormData)
+      .eq("id", id);
+    if (error) throw new Error(`Supabase error: ${error.message}`);
+    revalidatePath("/map");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateStartTime(formData: FormData) {
+  const rawFormData = {
+    start_time: formData.get("startTime"),
+  };
+  try {
+    const { error } = await supabase
+      .from("days")
+      .update(rawFormData)
+      .eq("id", 3);
+    if (error) throw new Error(`Supabase error: ${error.message}`);
+    revalidatePath("/map");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Delete //
+
+export async function deletePlace(place: PlaceT) {
+  try {
+    const { error } = await supabase.from("places").delete().eq("id", place.id);
+    if (error) throw new Error(`Supabase error: ${error.message}`);
+    revalidatePath("/map");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteTrip(id: number) {
+  const { error } = await supabase.from("trips").delete().eq("id", id);
+  if (error) throw new Error(`Supabase error: ${error.message}`);
+  revalidatePath("/trips");
 }

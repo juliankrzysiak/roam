0c1803b0fe2d609861798dc04f5b0650500552a8
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-05 21:12:22 -0800

    Fix form not closing on submit

--- src/features/trips/components/TripForm.tsx
+++ src/features/trips/components/TripForm.tsx
@@ -1,34 +1,45 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { createTrip } from "@/utils/actions";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { createTrip } from "@/utils/actions";
+import { useState } from "react";
 
 export default function TripForm() {
+  const [open, setOpen] = useState(false);
+
+  async function handleSubmit(formData: FormData) {
+    await createTrip(formData);
+    setOpen(false);
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger>
-        <Button>Add Trip</Button>
-      </DialogTrigger>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>Add Trip</DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Create New Trip</DialogTitle>
         </DialogHeader>
-        <form action={createTrip} className="flex flex-col items-center gap-2">
+        <form
+          action={handleSubmit}
+          id="createTrip"
+          className="flex flex-col items-center gap-2"
+        >
           <label className="flex w-full flex-col items-start gap-1">
             Name
-            <Input type="text" name="name" placeholder="Palmdale" />
+            <Input type="text" name="name" placeholder="Palmdale" required />
           </label>
         </form>
         <DialogFooter>
-          <Button type="submit">Submit</Button>
+          <Button form="createTrip">Submit</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>

f852687299d3308760000a28332165b5efed8f40
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-05 19:25:21 -0800

    Add svgr to work with svg easier

--- src/features/trips/components/TripOptions.tsx
+++ src/features/trips/components/TripOptions.tsx
@@ -2,16 +2,17 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+import MoreSVG from "@/assets/more-vertical.svg";
 
 export default function TripOptions() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="absolute right-2 top-2">
-        options
+        <MoreSVG />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>Edit</DropdownMenuItem>

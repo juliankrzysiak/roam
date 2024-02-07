dfe0581d82bc52a020a4dab92d030fc0f423e61f
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-06 21:06:10 -0800

    Delete old folder for maps

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-export const mapConfig: google.maps.MapOptions = {
-  center: { lat: 34.052, lng: -118.243 },
-  zoom: 10,
-  mapId: process.env.NEXT_PROCESS_MAP_ID,
-  mapTypeControl: false,
-  streetViewControl: false,
-  fullscreenControl: false,
-};
-

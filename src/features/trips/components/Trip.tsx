5d2c00895c256ebaee571133908d8abeed96f27d
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-26 21:55:37 -0800

    Style trip card

--- src/features/trips/components/Trip.tsx
+++ src/features/trips/components/Trip.tsx
@@ -14,12 +14,10 @@ type Props = Pick<Trip, "id" | "name">;
 
 export default async function Trip({ id, name }: Props) {
   return (
-    <Card className="relative flex flex-col items-center">
+    <Card className="relative flex flex-col items-center justify-between">
       <TripOptions id={id} />
       <CardHeader>
-        <CardTitle className="w-fit">
-          {name}
-        </CardTitle>
+        <CardTitle className="w-fit">{name}</CardTitle>
       </CardHeader>
       <CardContent>
         <p>Date</p>
@@ -33,4 +31,3 @@ export default async function Trip({ id, name }: Props) {
     </Card>
   );
 }
-

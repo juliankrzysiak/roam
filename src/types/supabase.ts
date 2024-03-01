67f93f26c026456ab2417d21abe4dca2c7659522
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-29 16:47:16 -0800

    Add command for generating types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,147 +4,159 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       days: {
         Row: {
-          created_at: string;
-          date: string | null;
-          id: string;
-          order_places: string[] | null;
-          start_time: string | null;
-          trip_id: number;
-        };
+          created_at: string
+          date: string | null
+          id: string
+          order_places: string[] | null
+          start_time: string | null
+          trip_id: number
+        }
         Insert: {
-          created_at?: string;
-          date?: string | null;
-          id?: string;
-          order_places?: string[] | null;
-          start_time?: string | null;
-          trip_id: number;
-        };
+          created_at?: string
+          date?: string | null
+          id?: string
+          order_places?: string[] | null
+          start_time?: string | null
+          trip_id: number
+        }
         Update: {
-          created_at?: string;
-          date?: string | null;
-          id?: string;
-          order_places?: string[] | null;
-          start_time?: string | null;
-          trip_id?: number;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          date?: string | null
+          id?: string
+          order_places?: string[] | null
+          start_time?: string | null
+          trip_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_days_trip_id_fkey"
+            columns: ["trip_id"]
+            isOneToOne: false
+            referencedRelation: "trips"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       places: {
         Row: {
-          category: string | null;
-          created_at: string;
-          day_id: string;
-          duration: number;
-          id: string;
-          lat: number;
-          lng: number;
-          name: string | null;
-        };
+          category: string | null
+          created_at: string
+          day_id: string
+          duration: number
+          id: string
+          lat: number
+          lng: number
+          name: string | null
+        }
         Insert: {
-          category?: string | null;
-          created_at?: string;
-          day_id: string;
-          duration?: number;
-          id: string;
-          lat: number;
-          lng: number;
-          name?: string | null;
-        };
+          category?: string | null
+          created_at?: string
+          day_id: string
+          duration?: number
+          id: string
+          lat: number
+          lng: number
+          name?: string | null
+        }
         Update: {
-          category?: string | null;
-          created_at?: string;
-          day_id?: string;
-          duration?: number;
-          id?: string;
-          lat?: number;
-          lng?: number;
-          name?: string | null;
-        };
-        Relationships: [];
-      };
+          category?: string | null
+          created_at?: string
+          day_id?: string
+          duration?: number
+          id?: string
+          lat?: number
+          lng?: number
+          name?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_places_day_id_fkey"
+            columns: ["day_id"]
+            isOneToOne: false
+            referencedRelation: "days"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       trips: {
         Row: {
-          created_at: string;
-          current_day: string | null;
-          id: number;
-          name: string;
-          order_days: string[];
-          user_id: string | null;
-        };
+          created_at: string
+          current_day: string | null
+          id: number
+          name: string
+          order_days: string[]
+          user_id: string | null
+        }
         Insert: {
-          created_at?: string;
-          current_day?: string | null;
-          id?: number;
-          name: string;
-          order_days?: string[];
-          user_id?: string | null;
-        };
+          created_at?: string
+          current_day?: string | null
+          id?: number
+          name: string
+          order_days?: string[]
+          user_id?: string | null
+        }
         Update: {
-          created_at?: string;
-          current_day?: string | null;
-          id?: number;
-          name?: string;
-          order_days?: string[];
-          user_id?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string
+          current_day?: string | null
+          id?: number
+          name?: string
+          order_days?: string[]
+          user_id?: string | null
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       get_places:
         | {
             Args: {
-              day: string;
-            };
+              day: string
+            }
             Returns: {
-              category: string | null;
-              created_at: string;
-              day_id: string;
-              duration: number;
-              id: string;
-              lat: number;
-              lng: number;
-              name: string | null;
-            }[];
+              category: string | null
+              created_at: string
+              day_id: string
+              duration: number
+              id: string
+              lat: number
+              lng: number
+              name: string | null
+            }[]
           }
         | {
             Args: {
-              day_id: number;
-            };
+              day_id: number
+            }
             Returns: {
-              category: string | null;
-              created_at: string;
-              day_id: string;
-              duration: number;
-              id: string;
-              lat: number;
-              lng: number;
-              name: string | null;
-            }[];
-          };
-    };
+              category: string | null
+              created_at: string
+              day_id: string
+              duration: number
+              id: string
+              lat: number
+              lng: number
+              name: string | null
+            }[]
+          }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
-
-export type Trip = Database["public"]["Tables"]["trips"]['Row'];
-export type NewTrip = Database["public"]["Tables"]["trips"]["Insert"];
-
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -153,11 +165,11 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -165,11 +177,11 @@ export type Tables<
       Database["public"]["Views"])
   ? (Database["public"]["Tables"] &
       Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
-  : never;
+  : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -177,20 +189,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
   ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
-  : never;
+  : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -198,20 +210,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
   ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
-  : never;
+  : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -219,9 +231,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
   ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never;
+  : never

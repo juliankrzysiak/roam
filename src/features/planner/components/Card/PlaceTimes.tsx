472fbc5156191808623c9603742294e4d2e477c1
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-03-26 00:16:57 -0700

    Add departure time

--- src/features/planner/components/Card/PlaceTimes.tsx
+++ src/features/planner/components/Card/PlaceTimes.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { PlaceT } from "@/types";
 import { updateDuration } from "@/utils/actions/crud/update";
-import { format } from "date-fns";
+import { add, format } from "date-fns";
+import { useState } from "react";
 
 const timeFormat = "HH:mm a";
 
@@ -13,17 +14,25 @@ type Props = {
 };
 
 export default function PlaceTimes({ arrival, duration, placeId }: Props) {
+  const [placeDuration, setDuration] = useState(duration);
+  const departure = add(arrival, { minutes: placeDuration });
+
   return (
-    <div>
+    <form className="flex flex-col  gap-2" action={updateDuration}>
       <p>Arrival: {format(arrival, timeFormat)}</p>
-      <form className="flex items-end gap-2" action={updateDuration}>
-        <label className="flex items-center gap-2">
-          Duration:
-          <Input className="px-1" name="duration" defaultValue={duration} />
-        </label>
-        <input type="hidden" name="id" defaultValue={placeId} />
-        <Button>Submit</Button>
-      </form>
-    </div>
+      <label className="flex items-center gap-2">
+        Duration:
+        <Input
+          className="px-1"
+          name="duration"
+          type="number"
+          defaultValue={placeDuration}
+          onChange={(e) => setDuration(Number(e.target.value))}
+        />
+      </label>
+      <input type="hidden" name="id" defaultValue={placeId} />
+      <Button>Submit</Button>
+      <p>Departure: {format(departure, timeFormat)}</p>
+    </form>
   );
 }

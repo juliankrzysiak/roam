45ddb5ec0201955e59f45b217a19b7a2b80337f8
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-18 23:31:12 -0700

    Add basic reorder icon

--- src/features/planner/components/Card/index.tsx
+++ src/features/planner/components/Card/index.tsx
@@ -1,8 +1,9 @@
 import { PlaceInfo } from "@/types";
-import { Reorder } from "framer-motion";
+import { Reorder, useDragControls } from "framer-motion";
 import PlaceTimes from "./PlaceTimes";
 import TripForm from "./TripForm";
 import { usePopupStore } from "@/lib/store";
+import { ReorderIcon } from "@/components/general/ReorderIcon";
 
 type Props = {
   place: PlaceInfo;
@@ -13,13 +14,20 @@ type Props = {
 export default function Card({ place, handleDragEnd, last }: Props) {
   const { id, arrival, placeDuration, tripDuration } = place;
   const updatePopup = usePopupStore((state) => state.updatePopup);
+  const controls = useDragControls();
 
   function onClickTitle() {
     updatePopup(place);
   }
 
   return (
-    <Reorder.Item value={place} id={place.id} onDragEnd={handleDragEnd}>
+    <Reorder.Item
+      value={place}
+      id={place.id}
+      dragListener={false}
+      dragControls={controls}
+      onDragEnd={handleDragEnd}
+    >
       <article className="flex flex-col gap-2 rounded-lg bg-slate-300 p-4 shadow-lg">
         <h1 className="font-bold underline" onClick={onClickTitle}>
           {place.name}
@@ -29,6 +37,7 @@ export default function Card({ place, handleDragEnd, last }: Props) {
           placeDuration={placeDuration}
           placeId={id}
         />
+        <ReorderIcon dragControls={controls} />
       </article>
       {!last && <TripForm placeId={id} tripDuration={tripDuration} />}
     </Reorder.Item>

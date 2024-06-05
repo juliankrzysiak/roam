50a7b15aee036e005b3e8cb4649a31cfcb7c2922
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-06-05 13:29:29 -0700

    Update style for time picker

--- src/features/map/components/MapDayControls.tsx
+++ src/features/map/components/MapDayControls.tsx
@@ -9,6 +9,7 @@ import { useOptimistic, useState } from "react";
 import { addMinutes, parse } from "date-fns";
 import { Separator } from "@/components/ui/separator";
 import { updateStartTime } from "@/utils/actions/crud/update";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   dayInfo: DayInfo;
@@ -17,13 +18,13 @@ type Props = {
 
 export default function DayControls({ dayInfo, totalDuration }: Props) {
   return (
-    <div className="absolute bottom-8 left-1/2 grid w-full max-w-xs -translate-x-1/2 grid-flow-col grid-rows-2 items-center gap-4 rounded-lg bg-background px-2 py-3 shadow-lg">
+    <div className="absolute bottom-8 left-1/2  w-full max-w-sm -translate-x-1/2 items-center gap-4 rounded-lg bg-background shadow-lg">
       <TimePicker dayInfo={dayInfo} totalDuration={totalDuration} />
-      <DatePicker
+      {/* <DatePicker
         initialDate={parseDate(dayInfo.date)}
         dayId={dayInfo.currentDayId}
-      />
-      <PlacePicker />
+      /> */}
+      {/* <PlacePicker /> */}
     </div>
   );
 }
@@ -36,27 +37,25 @@ function TimePicker({ dayInfo, totalDuration }: Props) {
   );
 
   return (
-    <div className="row-span-2 flex flex-col gap-2">
-      <form
-        className="flex items-center justify-between gap-2"
-        action={updateStartTime}
-      >
-        <label htmlFor="startTime" aria-label="Start Time">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-            />
-          </svg>
-        </label>
+    <form
+      className="flex items-center justify-between gap-2 p-2"
+      action={updateStartTime}
+    >
+      <label className="flex gap-2" aria-label="Start Time">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg>
         <Input
           type="time"
           id="startTime"
@@ -64,9 +63,9 @@ function TimePicker({ dayInfo, totalDuration }: Props) {
           defaultValue={startTime}
           onChange={(e) => setStartTime(e.target.value)}
         />
-        <button>Save</button>
         <input type="hidden" name="id" defaultValue={dayInfo.currentDayId} />
-      </form>
+      </label>
+
       <div className="flex items-center justify-between gap-2">
         <label htmlFor="endTime">
           <svg
@@ -84,13 +83,25 @@ function TimePicker({ dayInfo, totalDuration }: Props) {
             />
           </svg>
         </label>
-        <Input
-          type="time"
-          id="endTime"
-          defaultValue={format(endTime, "HH:mm")}
-        />
+        <span id="endTime">{format(endTime, "h:mm aa")}</span>
       </div>
-    </div>
+      <Button variant="outline" size="icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-4 w-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m4.5 12.75 6 6 9-13.5"
+          />
+        </svg>
+      </Button>
+    </form>
   );
 }
 

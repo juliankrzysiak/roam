4387cf514c0761ea47d618f6bed52bcf0538e96a
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-06-13 12:45:00 -0700

    Fix attribute to react syntax

--- src/features/map/components/MapControls.tsx
+++ src/features/map/components/MapControls.tsx
@@ -100,13 +100,13 @@ function TimePicker({ day, totalDuration }: TimePickerProps) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="h-4 w-4"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="m4.5 12.75 6 6 9-13.5"
           />
         </svg>

7bbfa1fd0fbae9d38d1131e8c9ced5297a580c0f
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-06-19 23:27:28 -0700

    Implement info window with search bar

--- src/features/map/components/MapSearch.tsx
+++ src/features/map/components/MapSearch.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useCurrentPlaceStore } from "@/lib/store";
 import { useMapsLibrary } from "@vis.gl/react-google-maps";
 import { useEffect, useRef, useState } from "react";
 
 export default function MapSearch() {
-  const [selectedPlace, setSelectedPlace] =
-    useState<google.maps.places.PlaceResult | null>(null);
+  const updateCurrentPlace = useCurrentPlaceStore(
+    (state) => state.updateCurrentPlace,
+  );
   const [placeAutocomplete, setPlaceAutocomplete] =
     useState<google.maps.places.Autocomplete | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -15,7 +17,7 @@ export default function MapSearch() {
     if (!places || !inputRef.current) return;
 
     const options = {
-      fields: ["place_id"],
+      fields: ["place_id", "geometry"],
     };
 
     setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
@@ -25,9 +27,16 @@ export default function MapSearch() {
     if (!placeAutocomplete) return;
 
     placeAutocomplete.addListener("place_changed", () => {
-      setSelectedPlace(placeAutocomplete.getPlace());
+      const place = placeAutocomplete.getPlace();
+      const placeId = place.place_id;
+      // Why do I need to call a function to get a number lol
+      const lat = place.geometry?.location?.lat();
+      const lng = place.geometry?.location?.lng();
+      if (!lat || !lng || !placeId) return;
+      const currentPlace = { placeId, position: { lat, lng } };
+      updateCurrentPlace({ currentPlace });
     });
-  }, [selectedPlace, placeAutocomplete]);
+  }, [placeAutocomplete]);
 
   return (
     <div className="absolute left-2 right-2 top-2 rounded-lg bg-slate-100 p-2 shadow-lg">

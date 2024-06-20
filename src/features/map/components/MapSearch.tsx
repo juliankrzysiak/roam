5035e01948edd13f51e6d7555fbea30a0f01760b
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-06-19 22:13:20 -0700

    Add autocomplete to search bar

--- src/features/map/components/MapSearch.tsx
+++ src/features/map/components/MapSearch.tsx
@@ -1,34 +1,37 @@
 "use client";
 
-import { searchText } from "@/utils/actions/api";
-import clsx from "clsx";
-import { Search } from "lucide-react";
-import { useState } from "react";
+import { useMapsLibrary } from "@vis.gl/react-google-maps";
+import { useEffect, useRef, useState } from "react";
 
 export default function MapSearch() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>(null);
+  const [placeAutocomplete, setPlaceAutocomplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary("places");
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ["place_id"],
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener("place_changed", () => {
+      setSelectedPlace(placeAutocomplete.getPlace());
+    });
+  }, [selectedPlace, placeAutocomplete]);
+
   return (
-    <form
-      action={searchText}
-      className={clsx(
-        "absolute left-2 top-2 flex items-center gap-2 rounded-full border bg-slate-100 p-2 shadow-lg sm:max-w-lg",
-        isOpen && "right-2",
-      )}
-    >
-      <Search
-        size={20}
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      />
-      {isOpen && (
-        <input
-          autoFocus
-          type="text"
-          name="search"
-          className="w-full bg-inherit"
-        />
-      )}
-    </form>
+    <div className="absolute left-2 right-2 top-2 rounded-lg bg-slate-100 p-2 shadow-lg">
+      <input ref={inputRef} className="w-full rounded-lg px-2" />
+    </div>
   );
 }

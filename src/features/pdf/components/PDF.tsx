774ebbc32c1387d3a47148893e11e248355c484d
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-07-29 13:30:47 -0700

    Add note section

--- src/features/pdf/components/PDF.tsx
+++ src/features/pdf/components/PDF.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Day } from "@/types";
+import { Day, Place } from "@/types";
 import {
   Document,
   Font,
@@ -71,54 +71,47 @@ export default function PDF({ days }: PDFProps) {
                 gap: 16,
               }}
             >
-              {day.places.map((place) => {
-                return (
-                  <View
-                    wrap={false}
-                    style={{ flexDirection: "row", gap: 4 }}
-                    key={place.id}
-                  >
-                    <View style={{ flexDirection: "column", gap: 4 }}>
-                      <View>
-                        <Text
-                          style={[
-                            styles.fontBase,
-                            { textDecoration: "underline" },
-                          ]}
-                        >
-                          {place.name}
-                        </Text>
-                        <Text style={styles.fontXs}>{place.address}</Text>
-                      </View>
-                      <View style={styles.fontXs}>
-                        <View>
-                          <Text>
-                            Arrival:{" "}
-                            {format(place.schedule.arrival, timeFormat)}
-                          </Text>
-                          <Text>Duration: {place.placeDuration}</Text>
-                          <Text>
-                            Departure:{" "}
-                            {format(place.schedule.departure, timeFormat)}
-                          </Text>
-                        </View>
-                        {place.travel && (
-                          <Text style={{ marginTop: 8 }}>
-                            Travel: {place.travel.duration} min
-                          </Text>
-                        )}
-                      </View>
-                    </View>
-                    <View>
-                      <Text>{place.notes}</Text>
-                    </View>
-                  </View>
-                );
-              })}
+              {day.places.map((place) => (
+                <PDFPlace key={place.id} place={place} />
+              ))}
             </View>
           </Page>
         ))}
       </Document>
     </PDFViewer>
+  );
+}
+
+type PlaceProps = {
+  place: Place;
+};
+
+function PDFPlace({ place }: PlaceProps) {
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View wrap={false} style={{ flexDirection: "column", gap: 4 }}>
+        <View>
+          <Text style={[styles.fontBase, { textDecoration: "underline" }]}>
+            {place.name}
+          </Text>
+          <Text style={styles.fontXs}>{place.address}</Text>
+        </View>
+        <View style={styles.fontXs}>
+          <View>
+            <Text>Arrival: {format(place.schedule.arrival, timeFormat)}</Text>
+            <Text>Duration: {place.placeDuration}</Text>
+            <Text>
+              Departure: {format(place.schedule.departure, timeFormat)}
+            </Text>
+          </View>
+          {place.travel && (
+            <Text style={{ marginTop: 8 }}>
+              Travel: {place.travel.duration} min
+            </Text>
+          )}
+        </View>
+      </View>
+      <Text style={styles.fontXs}>{place.notes}</Text>
+    </View>
   );
 }

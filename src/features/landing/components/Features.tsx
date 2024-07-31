1e262f5b842cd6e846fff6d872c07c9fac3f3b06
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-07-31 16:57:29 -0700

    Style features

--- src/features/landing/components/Features.tsx
+++ src/features/landing/components/Features.tsx
@@ -1,31 +1,30 @@
+import { volkhorn } from "@/app/fonts";
+
+const features = [
+  "Quickly create a scheduled itinerary for a fun day trip or an exciting journey across the states.",
+  "Get a bird's eye view of your trip. See all your stops on a map, know when your day will end, and see how many miles you'll go.",
+  "Print our trip details when you're done for those remote destinations.",
+  "Enjoy planning your trips for free!",
+];
+
 export default function Features() {
   return (
-    <section className=" grid gap-10 text-center text-2xl lg:grid-cols-2  lg:text-3xl">
-      <article className="max-w-lg rounded-lg bg-emerald-900 p-10 text-gray-100 lg:col-span-2">
-        <p>
-          Quickly create a scheduled itinerary for a fun day trip or an exciting
-          journey across the states.
-        </p>
-      </article>
-      <article className="max-w-lg rounded-lg  bg-gray-200 p-10 text-emerald-900 lg:col-start-2">
-        <p>
-          Get a bird&apos;s eye view of your trip. See all your stops on a map,
-          find out how many miles you&apos;ll go, know what the trip will cost,
-          and more.
-        </p>
-      </article>
-      <article className="max-w-lg rounded-lg bg-emerald-900 p-10 text-gray-100 lg:col-span-2">
-        <p>
-          Add your favorite places to lists. When you&apos;re done, simply drag
-          and drop them into your schedule.
-        </p>
-      </article>
-      <article className="max-w-lg rounded-lg bg-gray-200 p-10 text-emerald-900 lg:col-start-2">
-        <p>
-          Free to use, no account required. Or create one to save everything for
-          future inspiration!
-        </p>
-      </article>
+    <section className="flex w-full flex-col items-center gap-8 bg-emerald-900 p-8 ">
+      <h3 className="text-center text-4xl text-slate-100">
+        with <span className={volkhorn.className}>roam</span> you can
+      </h3>
+      <ul className="grid  gap-12 text-lg">
+        {features.map((feature) => {
+          return (
+            <li
+              key={feature}
+              className="max-w-md rounded-lg bg-slate-100 px-6 py-4 text-slate-900"
+            >
+              <p>{feature}</p>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }

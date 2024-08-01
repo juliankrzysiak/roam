713745bf1c7b27c0572f55731010961d2d9313ce
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-07-31 23:47:29 -0700

    Style background road

--- src/features/landing/components/Action.tsx
+++ src/features/landing/components/Action.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { isSignUpFormVisibleAtom } from "@/lib/atom";
 import { useSetAtom } from "jotai";
-import Link from "next/link";
 
 export default function Action() {
   const setOpen = useSetAtom(isSignUpFormVisibleAtom);
 
   return (
-    <section className="mb-12 flex flex-col items-center justify-center gap-8 px-8 text-center">
-      <div className="flex flex-col items-center gap-4">
+    <section className="mb-12 flex flex-col items-center justify-center gap-8  px-8 py-36 text-center">
+      <div className="relative flex flex-col items-center gap-4">
         <h2 className="font-display text-6xl text-emerald-950">
-          All Roads Lead to <span className="text-8xl font-medium">roam</span>
+          All Roads Lead to <br />
+          <span className="text-8xl font-medium">roam</span>
         </h2>
+
+        <div className="absolute top-1/2 -z-10 flex w-[3000px] rotate-45 flex-col gap-4 opacity-25 blur-[1px]">
+          <div className="flex h-12 w-full items-center border-y-2 border-slate-900">
+            <hr className="w-full  border-2 border-dashed border-slate-900" />
+          </div>
+          <div className="flex h-12 w-full items-center border-y-2 border-slate-900">
+            <hr className="w-full  border-2 border-dashed border-slate-900" />
+          </div>
+        </div>
       </div>
       <Button
         className="h-full w-full max-w-lg py-2  text-2xl text-slate-100"

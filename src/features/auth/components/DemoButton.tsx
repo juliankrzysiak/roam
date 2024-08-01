df495405f155f0b016303d4fef1afbb01c41978f
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-01 01:40:37 -0700

    Add gaurd clause for making anon account
    Stop people from spamming the butting if they already have an account,
    confirmed or not.

--- src/features/auth/components/DemoButton.tsx
+++ src/features/auth/components/DemoButton.tsx
@@ -7,9 +7,13 @@ export default function DemoButton() {
   const supabase = createClient();
 
   async function handleClick() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
     const { data, error } = await supabase.auth.signInAnonymously();
 
     try {
+      if (user) throw new Error("You already have an account.");
       if (error) throw new Error(error.message);
 
       router.push("/trips");

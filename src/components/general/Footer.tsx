59c7c05d8ea67af4445a9a478b9ffe08b18a5bfb
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-07-31 19:20:55 -0700

    Style footer

--- src/components/general/Footer.tsx
+++ src/components/general/Footer.tsx
@@ -1,8 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between p-4 text-lg font-bold text-emerald-900">
-      <a href="https://github.com/CastillejaCode">© 2023 Julian Krzysiak</a>
-      <a href="https://github.com/CastillejaCode/roam">Github</a>
+    <footer className="flex items-center justify-between gap-2 bg-emerald-900 p-4 text-sm text-slate-300">
+      <h4 className="font-display text-2xl">
+        <a href="#">roam</a>
+      </h4>
+      <div className="flex flex-col items-end gap-4 underline underline-offset-4 md:flex-row">
+        <a href="https://github.com/CastillejaCode/roam">Github</a>
+        <a href="https://juliankrzysiak.com">© 2024 Julian Krzysiak</a>
+      </div>
     </footer>
   );
 }

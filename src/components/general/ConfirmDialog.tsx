04b534687ea70281ce6935aa33119522814a8097
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-01 13:39:36 -0700

    Create confirm dialog for deletion

---
+++ src/components/general/ConfirmDialog.tsx
@@ -0,0 +1,40 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+
+type Props = {
+  children: React.ReactNode;
+  handleSubmit: () => void;
+};
+
+export default function ConfirmDialog({ children, handleSubmit }: Props) {
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent className="flex flex-col gap-8">
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>This action cannot be undone.</DialogDescription>
+        </DialogHeader>
+        <form className="flex justify-between" action={handleSubmit}>
+          <Button variant="destructive" type="submit">
+            Delete
+          </Button>
+          <DialogClose asChild>
+            <Button variant="secondary" type="button">
+              Go Back
+            </Button>
+          </DialogClose>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/features/user/components/DeleteForm.tsx
+++ src/features/user/components/DeleteForm.tsx
@@ -1,9 +1,12 @@
+import ConfirmDialog from "@/components/general/ConfirmDialog";
 import { Button } from "@/components/ui/button";
 
 export default function DeleteForm() {
   return (
     <div className="flex w-fit flex-col items-center gap-4">
-      <Button variant="destructive">Delete Data</Button>
+      <ConfirmDialog handleSubmit={() => console.log(123)}>
+        <Button variant="destructive">Delete Data</Button>
+      </ConfirmDialog>
       <Button variant="destructive">Delete Account + Data</Button>
     </div>
   );

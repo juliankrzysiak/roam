c3dbe74e7679bf4bbe1b1bb303b35e6b5b82499c
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-06 18:28:37 -0700

    Clean up imports

--- src/utils/actions/auth/index.ts
+++ src/utils/actions/auth/index.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 type AccountArgs = {

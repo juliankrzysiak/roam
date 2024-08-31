39057a1278b3f629b4c5d1568c2c3a60e6a479ef
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-30 20:26:51 -0700

    Add panning to first place when switching dates

--- src/features/map/components/MapDatePicker.tsx
+++ src/features/map/components/MapDatePicker.tsx
@@ -2,6 +2,8 @@
 
 import { DatePicker } from "@/components/general/DatePicker";
 import { DateRange, Day } from "@/types";
+import { useMap } from "@vis.gl/react-google-maps";
+import { useEffect } from "react";
 
 type MapControlsProps = {
   tripId: string;
@@ -14,6 +16,15 @@ export default function MapDatePicker({
   day,
   dateRange,
 }: MapControlsProps) {
+  const map = useMap();
+
+  useEffect(() => {
+    const firstLocation = day.places.at(0)?.position;
+    if (!map || !firstLocation) return;
+    map.panTo(firstLocation);
+    map.panBy(0, -150);
+  }, [day.id]);
+
   return (
     <div className="absolute bottom-6 left-1/2 -translate-x-1/2 rounded-lg border-2 border-emerald-900">
       <DatePicker

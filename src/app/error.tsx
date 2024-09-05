43b94fceec6e9293584aab00697477cc860418fa
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-09-05 01:41:55 -0700

    Add button to go home on error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { redirect, useRouter } from "next/navigation";
 
 export default function Error({
   error,
@@ -9,10 +10,18 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
   return (
-    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-4">
-      <h2 className="text-3xl">Something went wrong!</h2>
-      <Button onClick={() => reset()}>Try again</Button>
+    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-3xl">Something went wrong!</h2>
+        <h3 className="text-xl">{error.message}</h3>
+      </div>
+      <div className="flex flex-col items-center gap-1">
+        <Button onClick={() => reset()}>Try again</Button>
+        <span className="text-sm">or</span>
+        <Button onClick={() => router.push("/")}>Go Home</Button>
+      </div>
     </div>
   );
 }

9c937d4b05059498c849f588a6421208fbcdea7c
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-09-08 23:01:41 -0700

    Fix error page styling and add funny picture

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Error({
   error,
@@ -12,13 +12,20 @@ export default function Error({
 }) {
   const router = useRouter();
   return (
-    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-8">
-      <div className="flex flex-col items-center gap-2">
+    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-8 p-8">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <img
+          src="./sad_cat_potato.jpg"
+          className="mb-4 w-full max-w-md rounded-full"
+          alt="A sad cat that is a potato"
+        />
         <h2 className="text-3xl">Something went wrong!</h2>
-        <h3 className="text-xl">{error.message}</h3>
+        <p className="max-w-lg text-xl">{error.message}</p>
       </div>
       <div className="flex flex-col items-center gap-1">
-        <Button onClick={() => reset()}>Try again</Button>
+        <Button onClick={() => reset()} className="text-lg">
+          Try again
+        </Button>
         <span className="text-sm">or</span>
         <Button onClick={() => router.push("/")}>Go Home</Button>
       </div>

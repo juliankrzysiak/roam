dcc65c6a301865a4d4d7ffe9e7c5b15b4373a3a2
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-10-22 11:02:41 -0700

    Add info for each day of the trip

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import ShareTrip from "@/features/trips/components/ShareTrip";
 import TripOptions from "@/features/trips/components/TripOptions";
+import { Trip } from "@/types";
 import {
   convertTime,
   formatDateRange,
@@ -9,6 +10,7 @@ import {
   formatTripData,
 } from "@/utils";
 import { createClient } from "@/utils/supabase/server";
+import { format } from "date-fns";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
@@ -41,8 +43,8 @@ export default async function TripPage({ params }: Props) {
   };
 
   return (
-    <main className="mx-auto px-6 py-4">
-      <div className="relative flex w-fit flex-col items-center rounded-md border-2 border-slate-300 bg-slate-100 px-12 py-4">
+    <main className="grid place-items-center px-6 py-4">
+      <div className="relative flex w-full max-w-3xl flex-col items-center rounded-md border-2 border-slate-300 bg-slate-100 px-6 py-4">
         <Link
           href="/trips"
           className="absolute left-2 top-2 text-sm text-slate-500"
@@ -59,7 +61,7 @@ export default async function TripPage({ params }: Props) {
         />
         <h2 className="mb-2 text-3xl">{name}</h2>
         <div className="mb-4 flex flex-col gap-4">
-          <span className="text-xl">{formattedRange}</span>
+          <span className="text-lg">{formattedRange}</span>
           <div className="flex gap-2 ">
             <ShareTrip
               tripId={tripId}
@@ -75,15 +77,53 @@ export default async function TripPage({ params }: Props) {
             </Button>
           </div>
         </div>
-        <div className="flex w-full flex-col items-center gap-2">
-          <h3 className="text-xl">Totals</h3>
+        <div className="mb-4 flex w-full flex-col items-center gap-2">
+          <h3 className="text-2xl">Totals</h3>
           <div className="flex w-full flex-col items-center gap-1">
             <span> {totals.distance} miles</span>
             <span> {formatTotalDuration(totals.duration)} </span>
             <span> {totals.days} days</span>
           </div>
         </div>
+        <div className="flex w-full flex-col items-center gap-2">
+          <h3 className="text-2xl">Breakdown</h3>
+          <Days days={trip.days} />
+        </div>
       </div>
     </main>
+  );
+}
+
+function Days({ days }: { days: Trip["days"] }) {
+  return (
+    <ul className="flex w-full flex-col items-center gap-4">
+      {days.map((day, i) => {
+        const index = i + 1;
+        const date = format(day.date, "EEE, MMM dd");
+        const numberOfStops = day.orderPlaces.length + " places";
+        const distance = day.totals.distance + " miles";
+        const duration = formatTotalDuration(
+          convertTime({ minutes: day.totals.duration }),
+        );
+
+        return (
+          <li
+            key={day.date.toString()}
+            className="relative flex w-full max-w-sm flex-col items-center gap-2 rounded-sm border border-slate-500 px-4 py-2 odd:bg-slate-200"
+          >
+            <span className="absolute left-0 top-0 rounded-br-md border-b border-r border-slate-500 px-1">
+              {index}
+            </span>
+            <h4 className="text-lg font-semibold">{date}</h4>
+            <div className="flex flex-col items-center gap-0">
+              <span>{distance}</span>
+              <span>{duration}</span>
+              <span>{numberOfStops}</span>
+            </div>
+            <Button>Go To Date</Button>
+          </li>
+        );
+      })}
+    </ul>
   );
 }

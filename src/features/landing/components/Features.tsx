86a9e8fab8d882ae922c2a2319be11f50873ee2d
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-12-19 19:48:02 -0800

    Style features

--- src/features/landing/components/Features.tsx
+++ src/features/landing/components/Features.tsx
@@ -4,14 +4,14 @@ type Props = {
 
 export default function Features({ features }: Props) {
   return (
-    <ul className="grid w-full max-w-2xl grid-cols-magic gap-12 text-center text-2xl xl:py-8">
+    <ul className="grid w-full max-w-2xl grid-cols-magic gap-12 text-center text-xl xl:py-8">
       {features.map((feature) => {
         return (
           <li
             key={feature}
-            className="topography max-w-md rounded-lg bg-slate-100 px-6 py-4 text-slate-900 shadow-inner"
+            className="topography flex max-w-md items-center rounded-lg bg-slate-100 px-6 py-4 text-slate-900 shadow-inner"
           >
-            <p>{feature}</p>
+            <p className="text-center">{feature}</p>
           </li>
         );
       })}
